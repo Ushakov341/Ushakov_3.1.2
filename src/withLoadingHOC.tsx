405438ccd100d58d051ea0c9379329b38ac50627
Kirill <111366727+Ushakov341@users.noreply.github.com>
2025-10-22 09:52:07 +0200

    Updated withLoadingHOC.tsx

--- src/withLoadingHOC.tsx
+++ src/withLoadingHOC.tsx
@@ -1,1 +1,20 @@
-export function withLoading() {}
+import { ComponentType } from 'react';
+import { Loader } from './Loader';
+
+type WithLoadingProps = {
+  isLoading: boolean;
+};
+
+export function withLoading<P extends object>(
+  Component: ComponentType<P>
+) {
+  return function WithLoadingComponent(props: P & WithLoadingProps) {
+    const { isLoading, ...restProps } = props;
+
+    if (isLoading) {
+      return <Loader />;
+    }
+
+    return <Component {...(restProps as P)} />;
+  };
+}
